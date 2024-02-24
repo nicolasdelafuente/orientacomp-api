6f6fe353ecfdd634694963fdee4910902a8989e7
nicolas.delafuente <ni.delafuente@gmail.com>
2024-02-24 13:23:03 -0300

    Add conexión a la BD

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { sequelize } = require("./src/config/connect.js");
+
+const router = express.Router();
 
 const port = process.env.PORT || 4000;
-const publicUrl = process.env.PUBLIC_URL || 'http://localhost:'
+const publicUrl = process.env.PUBLIC_URL || "http://127.0.0.1:"
 
 app.use(cors());
+app.use(router);
 app.listen(port, () => {
-	console.log(`The server has started successfully at ${publicUrl}${port}`);
+    console.log(`The server has started successfully at ${publicUrl}${port}`);
+
+    sequelize.sync({ force: false }).then(() => {
+        console.log("The connection has been established");
+    });
 });
 
 module.exports = app;
---
+++ src/config/connect.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require('sequelize');
+const { config } = require('./config');
+
+const { database, username, password, host, dialect } = config;
+
+const sequelize = new Sequelize(
+	database,
+	username,
+	password, {
+	host: host,
+	dialect: dialect,
+}
+);
+
+const dbConnect = async () => {
+	try {
+		await sequelize.authenticate();
+		console.log("MySQL connected");
+	} catch (e) {
+		console.error("MySQL ERROR connected", e);
+	}
+};
+
+module.exports = { sequelize, dbConnect };
