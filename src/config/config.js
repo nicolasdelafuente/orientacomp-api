7954677f6ca2b732355b2579397889db0abd6308
nicolas.delafuente <ni.delafuente@gmail.com>
2024-02-25 15:45:49 -0300

    Add or a variables para configurar BD

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
 	config: {
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
-		host: process.env.DB_HOSTNAME,
-		dialect: process.env.DB_DIALECT,
+		username: process.env.DB_USERNAME || 'root',
+		password: process.env.DB_PASSWORD || '',
+		database: process.env.DB_NAME || 'orientacomp',
+		host: process.env.DB_HOSTNAME || 'localhost',
+		dialect: process.env.DB_DIALECT || 'mysql',
 	},
 };
