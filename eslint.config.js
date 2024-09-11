0eda34b7f1e1a0feb8a81fae5cdffdfb481623c9
nicolas.delafuente <ni.delafuente@gmail.com>
2024-09-11 08:12:30 -0300

    Cambio lint a commonJS

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ const globals = require('globals');
 const pluginJs = require('@eslint/js');
 const eslintPluginPrettier = require('eslint-plugin-prettier/recommended');
 
-export default [
+module.exports = [
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { languageOptions: { globals: globals.node } },
   { ignores: ['node_modules/'] },
