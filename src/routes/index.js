ebfd6fa202511c6609e453cd4a745a88f686df14
nicolas.delafuente <ni.delafuente@gmail.com>
2024-09-11 08:26:58 -0300

    Index para importar rutas dinamicas

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const router = express.Router();
+const routesPath = path.join(__dirname);
+
+fs.readdirSync(routesPath).forEach(file => {
+  if (file !== 'index.js' && file.endsWith('.js')) {
+    const routeName = file.split('.')[0];
+    console.log(`Loading route: ${routeName}`);
+    const route = require(path.join(routesPath, file));
+    router.use(`/${routeName}`, route);
+  }
+});
+
+module.exports = router;
