23f02d581a0c9b4e69a3e4e0de716fe86b7793be
adramarilla <adriana.amarilla@unahur.edu.ar>
2024-09-13 09:44:47 -0300

    OAP-16 Models Seguimiento

---
+++ src/models/Seguimiento.model.js
@@ -0,0 +1,57 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database.js');
+
+const Seguimiento = sequelize.define(
+  'Seguimiento',
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    id_tipo_de_seguimiento: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    id_categoria: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    id_estado: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    id_orienatdo: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    id_orienatdor: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    motivo: {
+      type: DataTypes.STRING(2048),
+      allowNull: false,
+    },
+    is_deleted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    deleted_by: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: 'Seguimientos',
+    timestamps: true,
+    paranoid: true,
+    underscored: true,
+  },
+);
+
+module.exports = Seguimiento;
