11c7e1e68ab55e2e1fb7cbebf9f9df723515e20e
nicolas.delafuente <ni.delafuente@gmail.com>
2024-09-17 23:08:32 -0300

    Elimino segunda opcion si no estan los .env

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -6,11 +6,11 @@ dotenv.config();
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '3306', 10),
-  database: process.env.DB_NAME || 'orientacompDevelopment',
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   logging: true,
   models: [path.join(__dirname, '/../models/*.model.js')],
   timezone: 'America/Argentina/Buenos_Aires',
