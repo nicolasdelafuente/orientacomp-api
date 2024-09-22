dd326d3b3991fae4ef6808146373571e45eb3819
nicolas.delafuente <ni.delafuente@gmail.com>
2024-09-22 09:32:10 -0300

    OAP-12 Add modelo Provincia

---
+++ src/database/models/Provincia.model.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database.js');
+const { commonModelOptions } = require('../config/modelOptions');
+
+const Provincia = sequelize.define(
+  'Provincia',
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id_pais: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    is_deleted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    ...commonModelOptions,
+    tableName: 'Provincias',
+  },
+);
+
+module.exports = Provincia;
