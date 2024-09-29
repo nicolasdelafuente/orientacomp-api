28380a356b504a848a95d6de335d4de61f071ff4
nicolas.delafuente <ni.delafuente@gmail.com>
2024-09-29 11:20:02 -0300

    Data para users

--- src/database/data/usuario.js
+++ src/database/data/usuario.js
@@ -1,3 +1,18 @@
-const usuario = [];
+const usuario = [
+  {
+    id_persona: 1,
+    id_rol: 1,
+    password: 'admin',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_persona: 2,
+    id_rol: 2,
+    password: 'orientador',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+];
 
 module.exports = usuario;
