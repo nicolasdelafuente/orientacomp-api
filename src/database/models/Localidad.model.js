be7dd41f2e20abb7bc34ba8e4c0c6abd4fb5a2b6
nicolas.delafuente <ni.delafuente@gmail.com>
2024-10-19 16:12:21 -0300

    Format code

---
+++ src/database/models/Localidad.model.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database.js');
+const { commonModelOptions } = require('../config/modelOptions.js');
+
+const Provincia = sequelize.define(
+  'Provincia',
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id_pais: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    is_deleted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    deleted_by: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  },
+  {
+    ...commonModelOptions,
+    tableName: 'Provincias',
+  },
+);
+
+module.exports = Provincia;
