00d4992997db3a6c6924c27863a31e8217f59d07
nicolas.delafuente <ni.delafuente@gmail.com>
2024-10-19 16:52:06 -0300

    Data entrevistas - seguimeintos

--- src/database/data/entrevista.js
+++ src/database/data/entrevista.js
@@ -1,3 +1,204 @@
-const entrevistas = [];
+const entrevistas = [
+  {
+    id_seguimiento: '1',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '2',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '2',
+    id_orientador: '2',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '2',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '3',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '5',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '4',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '4',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '6',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '7',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '8',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '9',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '10',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '9',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '8',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '8',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '9',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '6',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '4',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_seguimiento: '5',
+    id_orientador: '1',
+    observacion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    accion:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+];
 
 module.exports = entrevistas;
--- src/database/data/seguimiento.js
+++ src/database/data/seguimiento.js
@@ -1,3 +1,114 @@
-const seguimientos = [];
+const seguimientos = [
+  {
+    id_tipo_de_seguimiento: '1',
+    id_categoria: '1',
+    id_estado: '1',
+    id_orientado: '3',
+    id_orientador: '1',
+    motivo:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_tipo_de_seguimiento: '1',
+    id_categoria: '2',
+    id_estado: '2',
+    id_orientado: '4',
+    id_orientador: '1',
+    motivo:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_tipo_de_seguimiento: '1',
+    id_categoria: '3',
+    id_estado: '3',
+    id_orientado: '5',
+    id_orientador: '2',
+    motivo:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_tipo_de_seguimiento: '1',
+    id_categoria: '4',
+    id_estado: '4',
+    id_orientado: '6',
+    id_orientador: '2',
+    motivo:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_tipo_de_seguimiento: '1',
+    id_categoria: '5',
+    id_estado: '5',
+    id_orientado: '7',
+    id_orientador: '2',
+    motivo:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.   ',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_tipo_de_seguimiento: '1',
+    id_categoria: '6',
+    id_estado: '6',
+    id_orientado: '8',
+    id_orientador: '1',
+    motivo:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_tipo_de_seguimiento: '2',
+    id_categoria: '1',
+    id_estado: '7',
+    id_orientado: '9',
+    id_orientador: '2',
+    motivo:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_tipo_de_seguimiento: '2',
+    id_categoria: '2',
+    id_estado: '1',
+    id_orientado: '10',
+    id_orientador: '1',
+    motivo:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_tipo_de_seguimiento: '2',
+    id_categoria: '3',
+    id_estado: '2',
+    id_orientado: '3',
+    id_orientador: '2',
+    motivo:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id_tipo_de_seguimiento: '3',
+    id_categoria: '4',
+    id_estado: '3',
+    id_orientado: '2',
+    id_orientador: '1',
+    motivo:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+];
 
 module.exports = seguimientos;
